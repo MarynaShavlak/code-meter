f65b285f1639320973d726334312dca017dd6d81
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-02-12 22:43:24 +0200

    add full app logic

---
+++ js/questions.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    question: 'Яке значення забороняє виділяти текст?',
+    answer: 'user-select: none',
+    options: [
+      'no-select: none',
+      'pointer-select: none',
+      'user-select: unset',
+      'user-select: none',
+    ],
+  },
+  {
+    question:
+      'Який атрибут HTML5 вказує, що елемент недоступний для редагування?',
+    answer: 'readonly',
+    options: ['disabled', 'readonly', 'required', 'tabindex'],
+  },
+  {
+    question: 'Який із наступних атрибутів новий в HTML5?',
+    answer: 'checked',
+    options: ['autofocus', 'placeholder', 'required', 'checked'],
+  },
+  {
+    question: 'Значенням атрибуту border може бути:',
+    answer: '2',
+    options: ['top', 'left', '2', 'green'],
+  },
+  {
+    question: "Для чого потрібен атрибут autocomplete='off'?",
+    answer: 'откл автозаповнення',
+    options: [
+      'откл автозаповнення',
+      'вкл автозаповнення',
+      'робить input обовязковим для заповнення',
+      'скриває input',
+    ],
+  },
+];
